a93aa4ea53f4afc7f92cc5fc0fb5ad4ef1ecf493
Ivan Kachaunov <73343810+IvanKachaunov@users.noreply.github.com>
2022-05-17 02:03:41 +0300

    Add files via upload
    Some changes, but app is far from over

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
-// $(document).ready(function(){
-//     $('#To-Do').on('keyup', function(e){
-//         // 13 means other button
-//         if(e.keyCode === 13 && $('#To-Do'.val() != ""))
-//         {
-//             let task = $("div class='todo_list'></div>").text($('#To-Do').val());
-//             $("#Selector").append(task);
-//         }
-//     })
-
-// })
 
 $(document).ready(function() {
-    $(".To-DoButton").on("click", function(e) {
-      e.preventDefault();
-      var input = $("#To-Do").val()
-      $("#guests").html(input);
-    })
-  });
+
+$(".To-DoButton").click(function(e) {
+
+  e.preventDefault();
+
+  var input = $("#To-Do").val();
+
+    if($('.info-container li').length == 10){
+      alert("Warning, you reached your limit, please remove an element to continue!");
+    } else{
+      if($("#To-Do").val() == ""){
+        alert("Enter text!")
+      } else{
+      $(".info-container").append('<li>' + '<input type=checkbox class="checkbox">' + input + '<button id="update"></button>' + '<button id="delete-content"></button>' + '</li>').slideDown(200);
+      }
+    }
+
+})
+
+  $('.info-container').on('click', 'li', function(){
+    $(this).remove();
+  })
+
+  $('.info-container').on('click', '#update', function(){
+    alert("Can not be edited yet, check out for future updates!");
+  })
+
+  $('#delete').click(function(){
+    $("#To-Do").val('');
+    $("li").remove();
+  })
+
+})
